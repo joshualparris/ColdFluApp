d73a139b445aab79df7bd2db771850618818a9cd
Joshua Parris <joshua.parris@cornerstone.edu.au>
2026-07-15 09:19:24 +1000

    Remove imported preview and fix deployment build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
-const config = [...compat.extends("next/core-web-vitals", "next/typescript"), { ignores: [".next/**", "next-env.d.ts"] }];
+const config = [...compat.extends("next/core-web-vitals", "next/typescript"), { ignores: [".next/**", "inputs/**", "next-env.d.ts"] }];
 export default config;
